92f780f870a905a31211163ec27b3e085f18e182
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2023-05-22 15:14:01 +0300

    Update main.rs

--- rusty_gl/src/main.rs
+++ rusty_gl/src/main.rs
@@ -1,27 +1,30 @@
-use rusty_gl::rusty_gl::{*};
-use rusty_gl::sdl2_wrapper::{init_sdl_context, init_video_subsystem, init_window, init_canvas, init_event_pump, Lines};
 use sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Point;
 use std::time::Duration;
-use rand::prelude::*;
+
+pub mod rusty_gl;
+use rusty_gl::{*};
+
+pub mod sdl2_wrapper;
+use sdl2_wrapper::{*};
 
 fn main() {
     let mut points: Buffer = Buffer::init();
     let camera = Camera::init(Point3d { x: (0.0), y: (0.0), z: (0.0) }, 1000, 1000, 120.0, 0.1, 5000.0);
     let mut obj = Obj3d::init(
         &[
-            Point3d::init(-0.5, -0.5, 0.11),
-            Point3d::init(-0.5, 0.5, 0.11),
-            Point3d::init(0.5, -0.5, 0.11),
-            Point3d::init(0.5, 0.5, 0.11),
+            Point3d::init(-0.5, -0.5, 0.13),
+            Point3d::init(-0.5, 0.5, 0.13),
+            Point3d::init(0.5, -0.5, 0.13),
+            Point3d::init(0.5, 0.5, 0.13),
 
-            Point3d::init(-0.5, -0.5, 0.12),
-            Point3d::init(-0.5, 0.5, 0.12),
-            Point3d::init(0.5, -0.5, 0.12),
-            Point3d::init(0.5, 0.5, 0.12),
+            Point3d::init(-0.5, -0.5, 0.15),
+            Point3d::init(-0.5, 0.5, 0.15),
+            Point3d::init(0.5, -0.5, 0.15),
+            Point3d::init(0.5, 0.5, 0.1),
             ],
         &[
                 0, 1, 3, 2, 0,
@@ -32,7 +35,7 @@ fn main() {
             Color::RGB(1, 255, 1),
             Color::RGB(1, 1, 255)
     );
-    points.load_obj_to_buffer(&obj, &camera);
+    //points.load_obj_to_buffer(&obj, &camera);
     println!("{:#?}", points);
     
     let sdl_context = sdl2::init().unwrap();
@@ -48,9 +51,8 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    let mut rng = rand::thread_rng();
     'running: loop {
-        obj.rotate_x_axis(0.1);
+        obj.rotate_z_axis(0.1);
         points = Buffer::init();
         for event in event_pump.poll_iter() {
             match event {
@@ -69,6 +71,7 @@ fn main() {
         
         points.load_obj_to_buffer(&obj, &camera);
         // For performance, it's probably better to draw a whole bunch of points at once
+        canvas.draw_triangle([points.points[0], points.points[1], points.points[2]]);
         canvas.draw_lines_w(&points);
         canvas.draw_points_w(&points);
         //canvas.draw_points(points.points.as_slice()).unwrap();
